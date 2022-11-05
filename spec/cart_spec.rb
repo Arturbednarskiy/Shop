d8f504551d8b25119873eb3efe3c6d2497a67498
Artur Bednarskyi <arturbednarskiy@gmail.com>
2022-11-05 19:17:32 +0200

    context and pending tests

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -6,12 +6,19 @@ require_relative '../real_item'
 require_relative '../antique_item'
 
 describe Cart do
-  it 'adds items to cart' do
+
+  it 'Adds items to cart' do
     cart =  Cart.new(owner: 'dasha')
     item1 = Item.new(name: 'car', price: 300)
     item2 = Item.new(name: 'bike', price: 400)
-
     cart.add_item(item1, item2)
     expect(cart.items).to include(item1, item2)
+  end
+
+  context 'For example' do
+    it 'removes items' do; end
+    it 'counts'
+    it 'places order'
+    it 'clears itself'
   end
 end
